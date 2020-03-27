019e6ffe85a88d9e27b79a373a944301170e6f55
motform <love.lagerkvist@protonmail.com>
2020-03-27 20:27:37 +0100

    Add strain query

--- src/cocktail/slurp/db.clj
+++ src/cocktail/slurp/db.clj
@@ -1,13 +1,12 @@
 (ns cocktail.slurp.db
   (:require [cocktail.slurp.parse :as parse]
-            [datomic.api :as d]
-            [cocktail.slurp.db :as db]))
+            [datomic.api :as d]))
 
 ;;; db
 
 (def *conn (atom nil))
 
-(defn init-conn! [uri]
+(defn- init-conn! [uri]
   (reset! *conn (d/connect uri)))
 
 (defn init-db!
@@ -23,54 +22,59 @@
 
 ;; NOTE returns nil and not [] on fail
 (defn paginate [start limit q & args]
-  (let [result (apply (memoize q) args)
+  (let [result (apply q args)
         stop (+ start limit)]
     (when (seq result)
       (if (< stop (count result))
         (subvec result start stop)
         (subvec result start)))))
 
-(defn cocktail-by
-  "Factory for creating single attribute quires."
-  [a]
-  (fn [v]
-    (d/q '[:find [(pull ?e [*]) ...]
-           :in $ ?a ?v
-           :where [?e ?a ?v]]
-         (d/db @*conn) a v)))
-
-(def cocktail-by-id #(ffirst ((cocktail-by :id) %)))
-(def cocktail-by-title (cocktail-by :title))
-
-(defn cocktail-by-ingredients [ingredients]
-  (d/q '[:find [(pull ?e [*]) ...]
-         :in $ [?ingredients ...]
-         :where
-         [?e :ingredients ?ingredients]]
-       (d/db @*conn) ingredients))
+(defn cocktail-by-id [id]
+  (d/pull (d/db @*conn) '[:id :title :recipe :preparation :ingredients] [:id id]))
 
 (defn cocktail-feed []
-  (d/q '[:find [(pull ?e [:id :title :recipie :preparation :ingredients]) ...]
+  (d/q '[:find [(pull ?e [:id :title :recipe :preparation :ingredients]) ...]
          :where [?e :id]]
        (d/db @*conn)))
 
-(defn cocktail-by-fulltext [search]
-  (d/q '[:find [(pull ?e [:id :title :recipie]) ...]
-         :in $ ?search
-         :where [(fulltext $ :fulltext ?search) [[?e ?n]]]]
-       (d/db @*conn) search))
+(def base-query
+  '{:query {:find [(pull ?e [:id :title])]
+            :in [$]
+            :where []}
+    :args []})
+
+(defn- simple-query [q k s xs]
+  (-> q
+      (update-in [:query :in] conj [s '...])
+      (update-in [:query :where] conj ['?e k s])
+      (update-in [:args] conj xs)))
+
+(defn- fn-query [q k s f db xs]
+  (-> q
+      (update-in [:query :in] conj [s '...])
+      (update-in [:query :where] conj [`(~f ~db ~k ~s) '[[?e ?n]]])
+      (update-in [:args] conj xs)))
+
+(defn- parse-strainer [{:keys [:ingredients :search :type]}]
+  (cond-> base-query
+    ingredients (simple-query :ingredients '?ingredients ingredients)
+    type (simple-query :type '?type type)
+    search (fn-query :fulltext '?fulltext 'fulltext '$ search)))
+
+(defn strain [strainer]
+  (let [{:keys [query args]} (parse-strainer strainer)]
+    (apply d/q query (d/db @*conn) args)))
+
 
 (comment
   ;; datomic
   (init-db! {:uri "datomic:mem://cocktail.slurp"
              :posts "posts.edn"
              :schema "resources/edn/cocktail-schema.edn"})
+
   (d/delete-database "datomic:mem://cocktail.slurp")
 
-  (d/q '[:find (pull ?e [:id :title])
-         :where
-         [?e :id ?id]]
-       (d/db @*conn))
+  (strain {:ingredients ["rum" "cream"] :search ["russian"]})
 
   ;; export the cocktails
   (spit "resources/edn/formatted-posts.edn" (pr-str (parse/posts->cocktails "posts.edn"))))
--- src/cocktail/slurp/routes.clj
+++ src/cocktail/slurp/routes.clj
@@ -9,24 +9,29 @@
       (ring/response)
       (ring/header "Content-Type" "application/transit+json")))
 
-(defn strain-handler [{:keys [body]}]
-  (let [strainer (m/decode "application/transit+json" body)]
-    (transit+json-response (db/cocktail-by-title (:cocktail strainer)))))
-
-(defn cocktail-handler [{:keys [params]}]
-  (transit+json-response (db/cocktail-by-id (params "id"))))
-
-(defn cocktail-feed [{:keys [params]}]
-  (let [cocktails (-> params (get "cocktails") (Integer/parseInt))]
-    (transit+json-response (db/cocktail-feed cocktails))))
-
-(defn home-page [_]
+(defn- home-page [_]
   (ring/file-response "index.html" {:root "resources/public"}))
 
+(defn- strain [{:keys [body]}]
+  (let [strainer (m/decode "application/transit+json" body)]
+    (transit+json-response (db/paginate 0 20 db/strain strainer))))
+
+(defn- cocktail-by-id [{:keys [params]}]
+  (transit+json-response (db/cocktail-by-id (params "id"))))
+
+(defn- cocktail-feed [{:keys [params]}]
+  (let [start (-> params (get "start") (Integer/parseInt))
+        end (-> params (get "end") (Integer/parseInt))]
+    (transit+json-response (db/paginate start end db/cocktail-feed))))
+
+(defn- fulltext [{:keys [params]}]
+  (transit+json-response (db/cocktail-by-fulltext (get params "search"))))
+
 (def route-handler
-  (make-handler ["/" {"" home-page
-                      "index.html" home-page
-                      ;; "" (ring/resource-response "resources/public/index.html")
-                      "bartender/" {"strain" strain-handler
-                                    "cocktail" cocktail-handler
-                                    "cocktails/" {"feed" cocktail-feed}}}]))
+  (make-handler
+   ["/" {"" home-page
+         "index.html" home-page
+         "bartender/" {"strain" strain
+                       "cocktail" cocktail-by-id
+                       "cocktails/" {"feed" cocktail-feed
+                                     "fulltext" fulltext}}}]))
